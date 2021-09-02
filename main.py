e32afbfa81aae2256ac5151caf232f4770c2b5ec
Alex Incerti <a.incerti@iconsulting.biz>
2021-09-02 15:21:48 +0200

    loggings

--- main.py
+++ main.py
@@ -5,19 +5,24 @@ import os
 coins_to_exclude = ["LIQUIDITA'", "Total"]
 
 sheet = GoogleSheets(os.getenv("SHEET_ID"))
+print("-"*20)
 #
 print("Retrieving coins from google sheet...")
 coins = sheet.get_range("Crypto",1)
 coins.columns = ["coin"]
 coins = coins[1:] #remove header
 coins = coins[~(coins.coin == "") & ~(coins.coin.isin(coins_to_exclude))]
-print("Retrieved coins from google sheet")
+print(f"Retrieved coins from google sheet: [{','.join(coins.coin.values.tolist())}]")
+print("-"*20)
 #
 print("Retrieving prices from CoinMarketCap...")
 cmc = CoinMarketCap()
 df = cmc.get_prices(coins.coin.tolist(), "EUR")
 print("Retrieved prices from CoinMarketCap")
+print("-"*20)
 values = []
 for coin in coins.coin:
     values.append([df.at[coin]])
+print("Writing prices to Google Sheet...")
 sheet.write_to_range("Crypto", values, "C6:C")
+print("Wrote prices to Google Sheet")
